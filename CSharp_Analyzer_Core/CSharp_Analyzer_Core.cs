615b6a371d954647180e94dd96b512f08c7c064a
Kamin Fay <kaminjfay@gmail.com>
2020-07-25 01:11:15 -0400

    Created Core System + C# File Location Ingest

---
+++ CSharp_Analyzer_Core/CSharp_Analyzer_Core.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeAnalysis;
+using File_Ingest_System;
+
+namespace CSharp_Analyzer_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> CSharpFiles = Ingest.GenerateFileStructure(args);
+            
+            foreach (string file in CSharpFiles)
+            {
+              Console.Write("\n  Processing file {0}\n", System.IO.Path.GetFileName(file));
+            
+              CSsemi.CSemiExp semi = new CSsemi.CSemiExp();
+              semi.displayNewLines = false;
+              if (!semi.open(file as string))
+              {
+                Console.Write("\n  Can't open {0}\n\n", args[0]);
+                return;
+              }
+            
+              Console.Write("\n  Type and Function Analysis");
+              Console.Write("\n ----------------------------");
+            
+              BuildCodeAnalyzer builder = new BuildCodeAnalyzer(semi);
+              Parser parser = builder.build();
+            
+              try
+              {
+                while (semi.getSemi())
+                  parser.parse(semi);
+                Console.Write("\n  locations table contains:");
+              }
+              catch (Exception ex)
+              {
+                Console.Write("\n\n  {0}\n", ex.Message);
+              }
+              Repository rep = Repository.getInstance();
+              List<Elem> table = rep.locations;
+              Console.Write(
+                "\n  {0,10}, {1,25}, {2,5}, {3,5}, {4,5}, {5,5}, {6,5}, {7,5}",
+                "category", "name", "bLine", "eLine", "bScop", "eScop", "size", "cmplx"
+              );
+              Console.Write(
+                "\n  {0,10}, {1,25}, {2,5}, {3,5}, {4,5}, {5,5}, {6,5}, {7,5}",
+                "--------", "----", "-----", "-----", "-----", "-----", "----", "-----"
+              );
+              foreach (Elem e in table)
+              {
+                if (e.type == "class" || e.type == "struct")
+                  Console.Write("\n");
+                Console.Write(
+                  "\n  {0,10}, {1,25}, {2,5}, {3,5}, {4,5}, {5,5}, {6,5}, {7,5}", 
+                  e.type, e.name, e.beginLine, e.endLine, e.beginScopeCount, e.endScopeCount+1,
+                  e.endLine-e.beginLine+1, e.endScopeCount-e.beginScopeCount+1
+                );
+              }
+            
+              Console.Write("\n");
+              semi.close();
+            }
+            Console.Write("\n\n");
+            
+        }
+    }
+}
---
+++ File_Ingest_System/Ingest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mime;
+
+namespace File_Ingest_System
+{
+    public static class Ingest
+    {
+        private static string _projectDirectory;
+        private static List<string> _CSharpFiles;
+
+        public static string GetProjectDirectory()
+        {
+            return _projectDirectory;
+        }
+
+        public static void SetProjectDirectory(string dir)
+        {
+            if (Directory.Exists(dir))
+            {
+                _projectDirectory = dir;
+            }
+            else
+            {
+                throw new InvalidDataException("The directory you passed in does not exist, please try again");
+            }
+        }
+
+        public static List<string> SeparateCSharpFiles()
+        {
+            string[] filesInDirectory = Directory.GetFiles(_projectDirectory);
+            foreach (string fileName in filesInDirectory)
+            {
+                if (fileName.EndsWith(".cs") && !fileName.Contains("\\obj\\") && !fileName.Contains("\\AssemblyInfo"))
+                {
+                    Console.WriteLine(fileName);
+                    _CSharpFiles.Add(fileName);
+                }
+            }
+
+            string[] subDirectories = Directory.GetDirectories(_projectDirectory);
+            foreach (string subDirectory in subDirectories)
+            {
+                _projectDirectory = subDirectory;
+                SeparateCSharpFiles();
+            }
+
+            return _CSharpFiles;
+        }
+
+        public static List<string> GenerateFileStructure(string[] args)
+        {
+            _CSharpFiles = new List<string>();
+            try
+            {
+                SetProjectDirectory(args[0]);
+            }catch (Exception e)
+            {
+                switch (e)
+                {
+                    case IndexOutOfRangeException _:
+                        Console.WriteLine("Not enough arguments please try again");
+                        break;
+                    case InvalidDataException _:
+                        Console.WriteLine(e.Message);
+                        break;
+                }
+
+                System.Environment.Exit(1);
+            }
+            
+            return SeparateCSharpFiles();
+        }
+        
+        public static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+}
